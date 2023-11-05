0335b6321a4830b165481a47815dec35a3eef95a
Deivison Rocha <deivison.rocha@mercadolivre.com>
2023-11-05 16:47:55 -0300

    :sparkles: feat: add recurso para validação

--- src/model/publicacao.go
+++ src/model/publicacao.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -10,4 +14,27 @@ type Publicacao struct {
 	AutorNick string    `json:"autornick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaem,omitempty"`
+}
+
+func (p *Publicacao) Preparar() error {
+	if erro := p.validar(); erro != nil {
+		return erro
+	}
+	p.formatar()
+	return nil
+}
+
+func (p *Publicacao) validar() error {
+	if p.Titulo == "" {
+		return errors.New("O titulo é obrigatório e não pode estar em branco")
+	}
+	if p.Conteudo == "" {
+		return errors.New("O conteúdo é obrigatório e não pode estar em branco")
+	}
+	return nil
+}
+
+func (p *Publicacao) formatar() {
+	p.Titulo = strings.TrimSpace(p.Titulo)
+	p.Conteudo = strings.TrimSpace(p.Conteudo)
 }
